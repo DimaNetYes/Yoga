dfbb2b74be6dcb280669bbe8981dbe71ffa5ba8f
DiOne <realwindrunner@gmail.com>
2023-07-26 22:53:10 +0200

    added last style animation for main and fix oversiye bug with header nav

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -26,16 +26,16 @@
 
 
   <div id="container19" class="style4">
-    <div class="row ">
+    <div class="row" style="max-width: 100vw;">
         <!-- Text Column -->
-      <div class="col-lg-6 col-md-6 col-sm-12 order-lg-1 order-md-1 order-sm-2 order-1 custom-height">
+      <div class="col-lg-6 col-md-6 col-sm-12 order-lg-1 order-md-1 order-sm-1 order-2 custom-height">
         <div class="style1" style="padding:0 50px;">
           <h2 id="text12">Ihre Yoga-Reise beginnt hier</h2>
           <p id="text15" class="style3">Bei der Gründung von YYoga wollten wir, dass sich unsere Kunden hier so wohl wie möglich fühlen. Wir haben viel Mühe und Energie investiert, um einen schönen, komfortablen und technologisch fortschrittlichen Raum zu schaffen. Wenn Sie zu uns kommen, werden Sie sich immer als willkommener Gast fühlen.</p>
         </div>
       </div>
       <!-- Image Column -->
-      <div class="col-lg-6 col-md-6 col-sm-12 order-lg-2 order-md-2 order-sm-1 order-2 custom-height">
+      <div class="col-lg-6 col-md-6 col-sm-12 order-lg-2 order-md-2 order-sm-2 order-1 custom-height">
         <div class="style1" data-position="center">
           <span class="frame deferred">
             <img src="/img/Daniela.png" data-src="done" alt="" class="img-fluid">
