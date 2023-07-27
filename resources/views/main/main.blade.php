f7e491a715cddc903b6cae4ca1e1ac5f4f5a728e
DiOne <realwindrunner@gmail.com>
2023-07-27 10:47:44 +0200

    noch styles and html

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -26,7 +26,7 @@
         <div class="col-lg-6 col-md-6 col-sm-12 order-lg-1 order-md-1 order-sm-1 order-2 custom-height">
         <div class="style1" style="padding:20px 50px;">
             <h2 id="text12">Ihre Yoga-Reise beginnt hier</h2>
-            <p id="text15" class="style3">Bei der Gründung von YYoga wollten wir, dass sich unsere Kunden hier so wohl wie möglich fühlen. Wir haben viel Mühe und Energie investiert, um einen schönen, komfortablen und technologisch fortschrittlichen Raum zu schaffen. Wenn Sie zu uns kommen, werden Sie sich immer als willkommener Gast fühlen.</p>
+            <p id="text15" class="style3">Als wir Yoga eingeführt haben, wollten wir, dass sich unsere Kunden hier so wohl wie möglich fühlen. Bei uns werden Sie sich immer willkommen fühlen.</p>
         </div>
         </div>
         <!-- Image Column -->
@@ -47,8 +47,23 @@
   <div id="container01" class="style1 container default">
         <div class="wrapper">
             <div class="inner">
-                <h2 id="text17" class="style1">Accumsan sit amet</h2>
-                <p id="text49" class="style3">Quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum. Pretium quam vulputate dignissim suspendisse in est ante.</p>
+                <h2 id="text17" class="style1">Wir entscheiden uns für einen bewussten Lebensstil!</h2>
+                <p id="text49" class="style3">«Lass Frieden mit der Natur, Frieden mit den Menschen und Frieden in dir sein»</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="image11" class="style2 image">
+        <span class="frame deferred">
+            <img src="/img/logo3.png" data-src="done" alt="" style="opacity: 1;">
+        </span>
+    </div>
+
+    <div id="container01" class="style1 container default">
+        <div class="wrapper">
+            <div class="inner">
+                <h2 id="text17" class="style1">Das erste Studio seiner Art in Salzwedel.</h2>
+                <p id="text49" class="style3">Bei uns kannst du dich vom Yoga-Anfänger zum erfahrenen Praktiker und weiter zum Yogalehrer entwickeln</p>
             </div>
         </div>
     </div>
